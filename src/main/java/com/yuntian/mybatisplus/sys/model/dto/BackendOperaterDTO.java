ac65cd80b137e7a9c60022b63f6d80caa19450a0
yuntian <944610721@qq.com>
2019-06-25 23:43:23 +0800

    增加自定义模板生成

---
+++ src/main/java/com/yuntian/mybatisplus/sys/model/dto/BackendOperaterDTO.java
@@ -0,0 +1,15 @@
+package com.yuntian.mybatisplus.sys.model.dto;
+
+import java.io.Serializable;
+import com.yuntian.mybatisplus.common.BaseDTO;
+/**
+*
+* <p>
+    *  后台用户表
+    * </p>
+* @author yuntian
+* @since 2019-06-25
+*/
+public class BackendOperaterDTO extends BaseDTO implements Serializable{
+
+}
